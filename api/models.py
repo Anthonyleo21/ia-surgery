905b95d4ee6019a29d2f9761f7de61fad5a3b9f9
Anthonyleo21 <82071772+Anthonyleo21@users.noreply.github.com>
2023-11-25 01:09:38 -0500

    Update models.py
    decimals

--- api/models.py
+++ api/models.py
@@ -3,29 +3,28 @@ from django.db import models
 # Create your models here.
 
 class Surgery(models.Model):
-    #surgery = models.CharField(max_length=100)
     surgery = models.PositiveSmallIntegerField()
     hospital_number = models.PositiveSmallIntegerField()
-    age= models.PositiveSmallIntegerField()
-    reactal_temp= models.PositiveSmallIntegerField()
-    pulse= models.PositiveSmallIntegerField()
-    respiratory_rate = models.PositiveSmallIntegerField()
-    mucous_membrane =models.PositiveSmallIntegerField()
-    capillary_refill_time =models.PositiveSmallIntegerField()
-    PendingDeprecationWarningperistalsis =models.PositiveSmallIntegerField()
-    abdominal_distention =models.PositiveSmallIntegerField()
-    nasogastric_tube=models.PositiveSmallIntegerField()
-    nasogastric_reflux =models.PositiveSmallIntegerField()
-    nasogastric_reflux_ph =models.PositiveSmallIntegerField()
-    rectal_exam_feces =models.PositiveSmallIntegerField()
-    abdomen =models.PositiveSmallIntegerField()
-    packed_cell_volume =models.PositiveSmallIntegerField()
-    total_protein =models.PositiveSmallIntegerField()
-    abdomo_appearance= models.PositiveSmallIntegerField()
-    abdomo_protein =models.PositiveSmallIntegerField()
-    surgical_lesion =models.PositiveSmallIntegerField()
-    lesion_1=models.PositiveSmallIntegerField()
-    lesion_2=models.PositiveSmallIntegerField()
-    lesion_3=models.PositiveSmallIntegerField()
-    co_data=models.PositiveSmallIntegerField()
-    outcome=models.PositiveSmallIntegerField()
+    age = models.IntegerField()  # Permitir números negativos
+    reactal_temp = models.FloatField()  # Permitir decimales
+    pulse = models.IntegerField()
+    respiratory_rate = models.IntegerField()
+    mucous_membrane = models.PositiveSmallIntegerField()
+    capillary_refill_time = models.PositiveSmallIntegerField()
+    peristalsis = models.IntegerField()  # Permitir números negativos
+    abdominal_distention = models.IntegerField()
+    nasogastric_tube = models.IntegerField()
+    nasogastric_reflux = models.IntegerField()
+    nasogastric_reflux_ph = models.FloatField()  # Permitir decimales
+    rectal_exam_feces = models.IntegerField()
+    abdomen = models.IntegerField()
+    packed_cell_volume = models.FloatField()  # Permitir decimales
+    total_protein = models.FloatField()  # Permitir decimales
+    abdomo_appearance = models.IntegerField()
+    abdomo_protein = models.FloatField()  # Permitir decimales
+    surgical_lesion = models.IntegerField()
+    lesion_1 = models.IntegerField()
+    lesion_2 = models.IntegerField()
+    lesion_3 = models.IntegerField()
+    co_data = models.IntegerField()
+    outcome = models.IntegerField()
